d42154808feb872e499f9e6ba50c9f9bfa70fb4f
nguyen-hoang <nguyen.hoang@coxautoinc.com>
2024-02-13 03:27:50 +0700

    update

--- zalo-tet-api/Zalo.Tet.Api/Program.cs
+++ zalo-tet-api/Zalo.Tet.Api/Program.cs
@@ -1,16 +1,33 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add Swagger generation service
+builder.Services.AddSwaggerGen();
+builder.Services.AddMvcCore()
+        .AddApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
 }
 
 app.UseHttpsRedirection();
